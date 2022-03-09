481774f9e9744fb6d8ee71e0d05153b53237d4a3
Jost Trappmann <jost.trappmann@hebk.de>
2022-03-09 11:40:48 +0100

    n

--- Java/src/Schuelerverwaltung/src/Person.java
+++ Java/src/Schuelerverwaltung/src/Person.java
@@ -1,27 +1,13 @@
 public class Person {
-    private String vorname;
-    private String nachname;
     private int alter;
-    public Person (String pVorname, String pNachname, int pAlter){
-        vorname = pVorname;
-        nachname = pNachname;
+    public Person (int pAlter){
         alter = pAlter; 
     }
-    public void setVorname(String pName) {
-        vorname = pName;
-    }
-    public void setNachname(String pName) {
-        nachname = pName;
-    }
+
     public void setAlter(int pAlter){
         alter = pAlter;
     }
-    public String getVorname() {
-        return vorname;
-    }
-    public String getNachname() {
-        return nachname;
-    }
+
     public int getAlter(){
         return alter;
     }
