45f1752464d42aff9d8ca6ecf5dedd4451e403a5
GodnessBuddy <marvin.ruwe@hebk.de>
2022-03-16 12:24:30 +0100

    abzeichen klasse erstellt

---
+++ Java/src/Schuelerverwaltung/src/Abzeichen.java
@@ -0,0 +1,14 @@
+public class Abzeichen {
+    private String name;
+    private int level;
+    public Abzeichen(String pname, int plevel){
+        name=pname;
+        level=plevel;
+    }
+    public String getname(){
+        return name;
+    }
+    public int getlevel(){
+        return level;
+    }
+}
