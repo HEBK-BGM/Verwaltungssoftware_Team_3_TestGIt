db63f4400849fcc5306a14006a60fb0081575494
GodnessBuddy <marvin.ruwe@hebk.de>
2022-03-16 12:44:18 +0100

    Abzeichen geaendert

--- Java/src/Schuelerverwaltung/src/Abzeichen.java
+++ Java/src/Schuelerverwaltung/src/Abzeichen.java
@@ -1,12 +1,7 @@
 public class Abzeichen {
-    private String name;
     private int level;
-    public Abzeichen(String pname, int plevel){
-        name=pname;
+    public Abzeichen(int plevel){
         level=plevel;
-    }
-    public String getname(){
-        return name;
     }
     public int getlevel(){
         return level;
